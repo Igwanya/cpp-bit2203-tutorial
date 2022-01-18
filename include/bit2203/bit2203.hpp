8eb777a505feed84b36457b1b169ff1ba7dad945
Igwanya <felixmuthui32@gmail.com>
2022-01-18 16:04:02 +0300

    Added the bit2203 headers

---
+++ include/bit2203/bit2203.hpp
@@ -0,0 +1,27 @@
+#ifndef BIT2203_HPP
+#define BIT2203_HPP
+
+namespace bit2203 {
+
+  /**
+   * @brief converting gallons to cubic feet.
+   *
+   * The user enters the number of gallons and
+   * displays equivalent in cubic feet.
+   */
+  void exe_01();
+
+  /**
+   * @brief exe_02
+   */
+  void exe_02();
+
+  /**
+   * @brief exe_09
+   */
+  void exe_09();
+
+
+}
+
+#endif // BIT2203_HPP
